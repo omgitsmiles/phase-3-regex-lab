a84580ea8f691090bbb31c42e4f7876a693f7080
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-10-13 15:13:40 -0400

    commit tree 256bfb10cd2baa6f9197c0c0fdb46ea2e6173704

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,23 @@
 def starts_with_a_vowel?(word)
-
+    (/^[aeiou]/i).match?(word)
+    # bool i case insensitive ^ = first word
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/\bun\w*ing/)
+    # beings with un w * ends with ing
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
+    # b's only words 5 letters long
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    (/^[A-Z].+[.?!]$/).match?(text)
+    # end string is $ sign
 end
 
 def valid_phone_number?(phone)
-
+    /(\d{10})|(\(\d{3}\)\d{3}-\d{4})|\d{3} \d{3} \d{4}|(\(\d{3}\)\d{7})/.match?(phone)
 end
